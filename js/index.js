17c4f47042d031e949ee97f8ff5918f36aedde9d
April Sears <aprilnsears@gmail.com>
2025-01-18 21:59:43 -0500

    Provided examples in Index.js

--- js/index.js
+++ js/index.js
@@ -2,3 +2,35 @@
 // that are not declared
 "use strict"
 
+// Function
+//The name of the function.A list of parameters to the function, enclosed in parentheses and separated by commas.
+// The JavaScript statements that define the function, enclosed in curly braces, { /* … */ }.
+
+//function square(number) {return number * number; }
+
+
+//function myFunc(theObject) {theObject.make = "Toyota"; }
+  
+  /* const myCar = {
+    make: "Honda",
+    model: "Accord",
+    year: 1998,
+  };
+  
+  console.log(myCar.make); // "Honda"
+  myFunc(myCar);
+  console.log(myCar.make); // "Toyota"
+  
+*/
+
+/* function myFunc(theArr) {
+    theArr[0] = 30;
+  }
+  
+  const arr = [45];
+  
+  console.log(arr[0]); // 45
+  myFunc(arr);
+  console.log(arr[0]); // 30
+  */
+  
